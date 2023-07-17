c0f3aea91311bd1551225bae107b83b7b3ff8d77
ncece11 <nhiknhik14@gmail.com>
2023-07-18 01:49:22 +0800

    add views

--- index.js
+++ index.js
@@ -42,6 +42,30 @@ darkModeToggle.addEventListener('click', () => {
 });
 
 
+// Check if the 'viewsCount' key exists in localStorage
+if (localStorage.getItem('viewsCount')) {
+  // Retrieve the count from localStorage
+  var count = parseInt(localStorage.getItem('viewsCount'));
+  // Update the count value in the HTML element
+  document.getElementById('views-count').innerText = ' ' + count;
+} else {
+  // Set the initial count to 0
+  var count = 0;
+}
+
+// Increment the count by 1
+count++;
+
+// Update the count value in the HTML element
+document.getElementById('views-count').innerText = ' ' + count;
+
+// Store the updated count in localStorage
+localStorage.setItem('viewsCount', count.toString());
+
+// localStorage.clear(); //for clearing
+
+
+
 
 
 
